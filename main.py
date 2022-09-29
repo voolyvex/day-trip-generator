2c18eed0a23a3426d1733ce2f04f860388d69e17
voolyvex <1joeknapp@gmail.com>
2022-09-29 12:02:10 -0500

    create print current trip function and call it from the main function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 destinations = ["Shore Acres, Oregon", "Antarctica", "Golden Gate State Park", "Preparation Canyon", "Google Headquarters", "Chicago", "Boston", "Italy", "Little Italy", "Hawaiian island", "Aruba", "Kyoto, Japan", "Paris", "Iceland"]
 restaurants = ["4-star Michelin restaurant", "Bad Thai", "Taco Shelf", "Chunky Chicken Burgers", "La Buvette", "Panda Express", "Zio's Pizza", "La Casa", "El Mexican Restaurant", "Bangkok Thai Cuisine", "Matsu Sushi", "picnic lunch"]
@@ -6,16 +7,26 @@ entertainment = ["art museum", "nature hike", "ballet", "concert", "dance club",
 
 
 def generate_day_trip():
+    print_current_trip()
 
+def print_current_trip():
+    print("Destination: ", get_random_destination())
+    print("Restaurant: ", get_random_restaurant())
+    print("Mode of transportation: ", get_random_transportation())
+    print("Entertainment: ", get_random_entertainment())
 
 
 def get_random_restaurant():
+    pass
 def get_random_destination():
+    random_list_index = random.randrange(0, len(destinations))
+    return(destinations[random_list_index])
 def get_random_transportation():
+    pass
 def get_random_entertainment():
-
-def print_trip():
-
+    pass
+def print_final_trip():
+    pass
 
 if __name__ == "__main.py__":
     generate_day_trip()
