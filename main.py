5b59e5ea8746c3b14bb2d5e1e7af969e50d643b7
voolyvex <1joeknapp@gmail.com>
2022-09-29 10:53:47 -0500

    created function definition placeholders to be defined later

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ entertainment = []
 
 def generate_day_trip():
 
+def print_trip():
+
+def get_random_restaurant():
+def get_random_destination():
+def get_random_transportation():
+def get_random_entertainment():
 
 
 
