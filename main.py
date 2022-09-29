fe8e354aa72c4fee5994c62d19c17b1a141da308
voolyvex <1joeknapp@gmail.com>
2022-09-29 11:23:42 -0500

    populate the four lists with data to be randomly selected

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 
-destinations = []
-restaurants = []
-transportation = []
-entertainment = []
+destinations = ["Shore Acres, Oregon", "Antarctica", "Golden Gate State Park", "Preparation Canyon", "Google Headquarters", "Chicago", "Boston", "Italy", "Little Italy", "Hawaiian island", "Aruba", "Kyoto, Japan", "Paris", "Iceland"]
+restaurants = ["4-star Michelin restaurant", "Bad Thai", "Taco Shelf", "Chunky Chicken Burgers", "La Buvette", "Panda Express", "Zio's Pizza", "La Casa", "El Mexican Restaurant", "Bangkok Thai Cuisine", "Matsu Sushi", "picnic lunch"]
+transportation = ["bicycle", "train", "walking", "Uber", "school bus", "limousine", "skateboard", "kayak", "plane", "snowboard", "taxi", "motorcycle", "horseback", "riding lawnmower"]
+entertainment = ["art museum", "nature hike", "ballet", "concert", "dance club", "karaoke", "video game arcade", "miniature golf", "go to a movie", "state fair", "shopping", "architecture tour", "zoo", "scuba diving", "beach day", "go to the theatre"]
 
 
 def generate_day_trip():
 
-def print_trip():
+
 
 def get_random_restaurant():
 def get_random_destination():
 def get_random_transportation():
 def get_random_entertainment():
 
-
+def print_trip():
 
 
 if __name__ == "__main.py__":
